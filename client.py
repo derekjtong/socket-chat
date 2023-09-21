e3e5b99c87f83909ff62ffb4669ed9531594237c
Derek <109226053+derekjtong@users.noreply.github.com>
2023-09-21 01:00:37 -0400

    Added comments

--- client.py
+++ client.py
@@ -1,27 +1,42 @@
 import sys
 import socket
 
-
+# Check for the correct number of command-line arguments.
 if len(sys.argv) != 3:
     print(f"Usage: {sys.argv[0]} <host> <port>")
     sys.exit(1)
 
+# Extract the host and port from the command-line arguments.
 host, port = sys.argv[1], int(sys.argv[2])
+
+# Create a new socket for the client.
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the client socket to the specified server's host and port.
 client_socket.connect((host, port))
 
-name = input("Your name: ").strip()
+# Prompt the user for a username.
+name = input("Username: ").strip()
 
+# Continuously prompt the user to enter messages to send to the server.
 while True:
     message = input(f"{name}: ").strip()
+
+    # If the user doesn't enter a message, skip to the next iteration.
     if not message:
         continue
+
+    # Send the user's message to the server.
     client_socket.sendall(message.encode())
 
+    # If the user enters "exit", log the exit message and break the loop.
     if message == "exit":
         print("Exiting...")
         break
 
+    # Receive and print the server's reply.
     server_reply = client_socket.recv(1024).decode()
     print(server_reply)
+
+# Close the client socket after communication ends.
 client_socket.close()
--- server.py
+++ server.py
@@ -5,47 +5,71 @@ import uuid
 
 
 def link_handler(link, client, id):
+    """
+    Handle individual client connections in separate threads.
+
+    :param link: The socket connection to the client.
+    :param client: A tuple containing the client's IP and port.
+    :param id: A unique UUID assigned to the client.
+    """
+    # Get the native thread ID for logging purposes.
     thread_id = threading.get_native_id()
+
+    # Log the new connection and the thread handling it.
     print(
         "[SERVER]: New connection from [%s:%s], created new thread [%s]"
         % (address[0], address[1], thread_id)
     )
     print(" [%s]: Client [%s:%s] connected" % (thread_id, client[0], client[1]))
+
+    # Continuously listen for messages from the client.
     while True:
         client_data = link.recv(1024).decode()
         if client_data == "exit":
+            # If the client sends "exit", log the disconnection and break the loop.
             print(
                 " [%s]: Client [%s:%s] ended communication"
                 % (thread_id, client[0], client[1])
             )
             break
+        # Log the received message from the client.
         print(
-            " [%s]: Client [%s:%s] sent message：%s"
+            " [%s]: Client [%s:%s] sent message: %s"
             % (thread_id, client[0], client[1], client_data)
         )
         try:
+            # Send a response back to the client with its UUID.
             link.sendall(f"[{thread_id}] Server: Your UUID is {id}".encode())
         except BrokenPipeError:
+            # Handle the case where the client forcibly closes the connection.
             print(
                 " [%s]: Client [%s:%s] forcibly closed the connection."
                 % (thread_id, client[0], client[1])
             )
             break
+    # Close the connection to the client.
     link.close()
 
 
+# Check for the correct number of command-line arguments.
 if len(sys.argv) != 3:
     print(f"Usage: {sys.argv[0]} <host> <port>")
     sys.exit(1)
 
+# Extract the host and port from the command-line arguments.
 host, port = sys.argv[1], int(sys.argv[2])
+
+# Create a new socket and bind it to the specified host and port.
 sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sk.bind((host, port))
 sk.listen()
 
+# Log that the server has started and is listening for connections.
 print("[SERVER]: Started listening on [%s:%s], awaiting clients..." % (host, port))
 
+# Continuously listen for incoming client connections.
 while True:
     conn, address = sk.accept()
+    # Start a new thread to handle the client connection.
     t = threading.Thread(target=link_handler, args=(conn, address, uuid.uuid4()))
     t.start()
